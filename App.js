611d26c77e412a605ffccc42e3d72666d26e6362
rajaonahsoa <r.lalainafr@gmail.com>
2023-11-25 13:37:52 +0100

    Navigation

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
 import About from './components/About';
 import Search from './components/Search';
+import {NavigationContainer} from "@react-navigation/native";
+import {createMaterialTopTabNavigator} from "@react-navigation/material-top-tabs";
+
+const Tab = createMaterialTopTabNavigator();
 
 export default function App() {
   return (
-    <View>
-      <About />
-      <Search />
-    </View>
-  );
+    <NavigationContainer>
+      <Tab.Navigator tabBarPosition='bottom' style={{marginVertical: 30}}>
+        <Tab.Screen name='About' component={About} />
+        <Tab.Screen name='Search' component={Search} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
 }
 
 
